63ae29b8c8dda8798177edc289fabd72e7106503
Michael Levitski <levitski.michael@gmail.com>
2020-01-11 16:30:22 -0800

    add login page and local bg image

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ addEventListener('load',async()=>{
         date='today';
     }
 
-    document.body.style.backgroundImage='url(https://ca-ga-static-content.perfectmind.com/Content/Images/landing-page-bg.jpg?v=2)';
     document.body.style.backgroundPosition='center';
     document.body.style.backgroundSize='cover';
-    
+    document.body.style.backgroundImage='url(../bg.jpg)';
+
     let courses=(await(await fetch('https://regbadminton.com/api/?d='+date)).json()).map(obj=>new Course(obj));
     if(page>courses.length)location='https://cityofsurrey.perfectmind.com/23615/Menu/BookMe4BookingPages/Classes?calendarId=ec6defcd-4317-4bf3-a72e-a9c6b4e5c897&widgetId=15f6af07-39c5-473e-b053-96653f77a406&embed=False';
     else location=courses[page-1].getURL();
