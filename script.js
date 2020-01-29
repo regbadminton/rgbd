6ba3321175f4e7c3bddf4d330372b958349cd58c
Michael Levitski <levitski.michael@gmail.com>
2020-01-29 00:46:31 -0800

    fix spacing

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 addEventListener('load', async()=>{
 
-    const pathname = location.pathname;
-    const last = pathname.lastIndexOf('/');
-    let page = pathname.substring(last - 1,last);
-    let date = 'tomorrow';
+  const pathname = location.pathname;
+  const last = pathname.lastIndexOf('/');
+  let page = pathname.substring(last - 1,last);
+  let date = 'tomorrow';
 
-    if (isNaN(page)){
-        page = page.charCodeAt(0) - 96;
-        date = 'today';
-    }
+  if (isNaN(page)){
+    page = page.charCodeAt(0) - 96;
+    date = 'today';
+  }
 
-    document.body.style.backgroundPosition = 'center';
-    document.body.style.backgroundSize = 'cover';
-    document.body.style.backgroundImage = 'url(../bg.jpg)';
+  document.body.style.backgroundPosition = 'center';
+  document.body.style.backgroundSize = 'cover';
+  document.body.style.backgroundImage = 'url(../bg.jpg)';
 
-    const courses = await(await fetch('https://regbadminton.com/api/?d=' + date)).json();
+  const courses = await(await fetch('https://regbadminton.com/api/?d=' + date)).json();
 
-    if (page > courses.length) location = 'https://cityofsurrey.perfectmind.com/23615/Menu/BookMe4BookingPages/Classes'+
-                                            '?calendarId=ec6defcd-4317-4bf3-a72e-a9c6b4e5c897'+
-                                            '&widgetId=15f6af07-39c5-473e-b053-96653f77a406'+
-                                            '&embed=False';
+  if (page > courses.length) location = 'https://cityofsurrey.perfectmind.com/23615/Menu/BookMe4BookingPages/Classes'+
+                                          '?calendarId=ec6defcd-4317-4bf3-a72e-a9c6b4e5c897'+
+                                          '&widgetId=15f6af07-39c5-473e-b053-96653f77a406'+
+                                          '&embed=False';
 
-    else location = `https://cityofsurrey.perfectmind.com/23615/Menu/BookMe4EventParticipants`+
-                      `?eventId=${courses[page - 1].classID}`+
-                      `&occurrenceDate=${courses[page - 1].date.replace(/-/g, '')}`+
-                      `&widgetId=15f6af07-39c5-473e-b053-96653f77a406`+
-                      `&locationId=${courses[page - 1].locationID}`+
-                      `&waitListMode=False`;
+  else location = `https://cityofsurrey.perfectmind.com/23615/Menu/BookMe4EventParticipants`+
+                    `?eventId=${courses[page - 1].classID}`+
+                    `&occurrenceDate=${courses[page - 1].date.replace(/-/g, '')}`+
+                    `&widgetId=15f6af07-39c5-473e-b053-96653f77a406`+
+                    `&locationId=${courses[page - 1].locationID}`+
+                    `&waitListMode=False`;
 });
